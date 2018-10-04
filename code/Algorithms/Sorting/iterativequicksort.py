79c6a5de2fc2f7d0d78a5e70ea3570254f14dd35
Saphal patro <saphal1998@gmail.com>
2018-10-04 16:11:48 +0530

    Added iterative quick sort

---
+++ code/Algorithms/Sorting/iterativequicksort.py
@@ -0,0 +1,17 @@
+# The main function that implements QuickSort 
+# arr[] --> Array to be sorted, 
+# low --> Starting index, 
+# high --> Ending index 
+  
+# Function to do Quick sort 
+def quickSort(arr,low,high): 
+    if low < high: 
+  
+        # pi is partitioning index, arr[p] is now 
+        # at right place 
+        pi = partition(arr, low, high) 
+  
+        # Separately sort elements before 
+        # partition and after partition 
+        quickSort(arr, low, pi-1) 
+        quickSort(arr, pi+1, high) 
